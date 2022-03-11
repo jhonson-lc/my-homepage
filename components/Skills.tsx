532b1fbfc3874c20b7bff7abbf2b068c793a9efd
jhonson-lc <jhoncisnero11@gmail.com>
2022-03-11 12:30:13 -0500

    "refactor: reestructuring"

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -7,11 +7,11 @@ interface Props {}
 
 const Skills: React.FC<Props> = () => {
   return (
-    <Stack direction="row" gap={6} justifyContent="center" w="100%" wrap="wrap">
+    <Stack direction="row" gap={3} justifyContent="center" w="100%" wrap="wrap">
       {skills.map((skill) => {
         return (
           <SkillImage
-            key={skill.image}
+            key={skill.stack}
             image={skill.image}
             name={skill.stack}
           />
@@ -24,7 +24,8 @@ const Skills: React.FC<Props> = () => {
 const SkillImage = ({ image, name }) => {
   return (
     <Stack alignItems="center" direction="column">
-      <Image h={12} src={image} w={12} />
+      {image}
+      {image.length && <Image h={12} src={image} w={12} />}
       <Text fontSize="xs" variant="information">
         {name}
       </Text>
--- components/constants.tsx
+++ components/constants.tsx
@@ -1,9 +1,12 @@
+import NextJS from "../ui/icons/Next";
+
 export const skills = [
   { stack: "Chakra-UI", image: "../images/skills/1.svg" },
   { stack: "Css", image: "../images/skills/2.svg" },
   { stack: "Html", image: "../images/skills/3.svg" },
   { stack: "Javascript", image: "../images/skills/4.svg" },
-  { stack: "Next.js", image: "../images/skills/5.svg" },
+  { stack: "Next.js", image: <NextJS /> },
   { stack: "React", image: "../images/skills/6.svg" },
   { stack: "Styled Components", image: "../images/skills/7.svg" },
+  { stack: "Typescript", image: "../images/skills/8.svg" },
 ];
