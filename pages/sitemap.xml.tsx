680b12491a8eecf8413ba593cd292f0ade0188cb
jhonson-lc <jhoncisnero11@gmail.com>
2022-03-31 13:02:50 -0500

    "fix: sitemap"

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -3,31 +3,34 @@ import { singleBlog } from "../blog/types";
 
 export function generateSiteMap(blogs: singleBlog[]) {
   return `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <urlset xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
           <loc>https://mejhon.dev</loc>
+          <lastmod>2022-03-31T17:58:58+00:00</lastmod>
+          <priority>1.00</priority>
       </url>
-      <url>
-            <loc>https://mejhon.dev/</loc>
-            <lastmod>2022-03-24T21:24:19+00:00</lastmod>
-        </url>
         <url>
             <loc>https://mejhon.dev/work</loc>
-            <lastmod>2022-03-24T21:24:19+00:00</lastmod>
+            <lastmod>2022-03-31T17:58:58+00:00</lastmod>
+            <priority>0.80</priority>
         </url>
         <url>
             <loc>https://mejhon.dev/blog</loc>
             <lastmod>2022-03-24T21:24:19+00:00</lastmod>
+            <priority>0.80</priority>
         </url>
         <url>
             <loc>https://mejhon.dev/contact</loc>
             <lastmod>2022-03-24T21:24:19+00:00</lastmod>
+            <priority>0.80</priority>
         </url>
       ${blogs
         .map((blog) => {
           return `
               <url>
                   <loc>${`https://mejhon.dev/blog/${blog?.properties.slug.rich_text[0]?.plain_text}`}</loc>
+                  <lastmod>2022-03-24T21:24:19+00:00</lastmod>
+                  <priority>0.80</priority>
               </url>
           `;
         })
