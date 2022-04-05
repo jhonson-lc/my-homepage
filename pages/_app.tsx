6af271c4ff258964c37423d7a019c1d1d1dbe699
jhonson-lc <jhoncisnero11@gmail.com>
2022-04-05 12:22:31 -0500

    "refactor: fix fonts"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,11 @@ import { NextPage } from "next";
 
 import Layout from "../app/layouts/IndexLayout";
 import Chakra from "../ui/structure/chakra";
-import Fonts from "../lib/fonts";
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
     <Chakra>
       <Layout>
-        <Fonts />
         <AnimatePresence exitBeforeEnter initial={false}>
           <Component {...pageProps} />
         </AnimatePresence>
