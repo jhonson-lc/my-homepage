a350cbba332546ec045b1afde29654e6dd4ac4f9
jhonson-lc <jhoncisneros11@gmail.com>
2022-06-22 23:04:38 -0500

    fix: sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,36 +1,16 @@
-import api from "../blog/resources";
+import { sanityClient } from "lib/sanity-server";
+import { postSlugsQuery } from "lib/queries";
+
 import { singleBlog } from "../blog/types";
 
-export function generateSiteMap(blogs: singleBlog[]) {
+export function createSitemap(blogs: singleBlog[]) {
   return `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
-          <loc>https://mejhon.dev</loc>
-          <lastmod>2022-03-31T17:58:58+00:00</lastmod>
-          <priority>1.00</priority>
-      </url>
-        <url>
-            <loc>https://mejhon.dev/work</loc>
-            <lastmod>2022-03-31T17:58:58+00:00</lastmod>
-            <priority>0.80</priority>
-        </url>
-        <url>
-            <loc>https://mejhon.dev/blog</loc>
-            <lastmod>2022-03-24T21:24:19+00:00</lastmod>
-            <priority>0.80</priority>
-        </url>
-        <url>
-            <loc>https://mejhon.dev/contact</loc>
-            <lastmod>2022-03-24T21:24:19+00:00</lastmod>
-            <priority>0.80</priority>
-        </url>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9>
       ${blogs
         .map((blog) => {
           return `
               <url>
-                  <loc>${`https://mejhon.dev/blog/${blog?.properties.slug.rich_text[0]?.plain_text}`}</loc>
-                  <lastmod>2022-03-24T21:24:19+00:00</lastmod>
-                  <priority>0.80</priority>
+                  <loc>${`https://mejhon.dev/${blog?.properties.slug.rich_text[0]?.plain_text}`}</loc>
               </url>
           `;
         })
@@ -39,17 +19,19 @@ export function generateSiteMap(blogs: singleBlog[]) {
   `;
 }
 
-function SiteMap() {}
-
 export async function getServerSideProps({ res }) {
-  // We make an API call to gather the URLs for our site
-  const blogs = await api.list();
-
-  // We generate the XML sitemap with the posts data
-  const sitemap = generateSiteMap(blogs);
+  const allPosts = await sanityClient.fetch(postSlugsQuery);
+  const allPages = [
+    ...allPosts.map((slug) => `blog/${slug}`),
+    ...["", "contact", "blog", "work"],
+  ];
 
   res.setHeader("Content-Type", "text/xml");
-  res.write(sitemap);
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=1200, stale-while-revalidate=600",
+  );
+  res.write(createSitemap(allPages));
   res.end();
 
   return {
@@ -57,4 +39,6 @@ export async function getServerSideProps({ res }) {
   };
 }
 
-export default SiteMap;
+export default function SiteMap() {
+  return null;
+}
