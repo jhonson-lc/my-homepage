76d341bebe8be42203185378a1765d8164491b8e
jhonson-lc <jhoncisneros11@gmail.com>
2024-06-11 21:53:55 -0500

    Refactor ContactScreen component

--- contact/screens/ContactScreen.tsx
+++ contact/screens/ContactScreen.tsx
@@ -4,13 +4,11 @@ import Avatar from "ui/feedback/Avatar";
 
 import InputInformation from "../components/InputInformation";
 import Layout from "../../app/layouts/Layout";
+import ContactForm from "contact/components/ContactForm";
 
 export default function ContactScreen() {
   return (
-    <Layout
-      description="Contact me to work together"
-      title="Contact - Jhon Lescano"
-    >
+    <Layout description="Contact me to work together" title="Contact - Jhon Lescano">
       <Stack
         align="center"
         direction={{ base: "column", sm: "row" }}
@@ -28,27 +26,17 @@ export default function ContactScreen() {
             Contact
           </Heading>
           <Text color="secondary">Fill up the form below to contact</Text>
-          <Stack
-            alignItems="flex-start"
-            direction="column"
-            justifyContent="flex-start"
-            spacing={3}
-          >
-            <InputInformation icon={<MdPhone />}>
-              +593 98 165 7629
-            </InputInformation>
-            <InputInformation icon={<MdEmail />}>
-              antibots@gmail.com
-            </InputInformation>
-            <InputInformation icon={<MdLocationOn />}>
-              Quito, Ecuador
-            </InputInformation>
+          <Stack alignItems="flex-start" direction="column" justifyContent="flex-start" spacing={3}>
+            <InputInformation icon={<MdPhone />}>+593 98 165 7629</InputInformation>
+            <InputInformation icon={<MdEmail />}>antibots@gmail.com</InputInformation>
+            <InputInformation icon={<MdLocationOn />}>Quito, Ecuador</InputInformation>
           </Stack>
         </Stack>
         <Box pr={{ base: 0, md: 12, lg: 100 }}>
           <Avatar />
         </Box>
       </Stack>
+      <ContactForm />
     </Layout>
   );
 }
