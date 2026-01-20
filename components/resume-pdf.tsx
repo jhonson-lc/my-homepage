c65fa364183809a7531f43022ee49c2af50a3b00
jhonson-lc <jhoncisneros11@gmail.com>
2026-01-20 12:05:39 -0500

    Fix curriculum to ATS

---
+++ components/resume-pdf.tsx
@@ -0,0 +1,327 @@
+import React from "react";
+import { Document, Page, Text, View, StyleSheet, Link, Image } from "@react-pdf/renderer";
+import { WorkType, EducationType, NaturalLanguageType, ProjectType } from "../app/types";
+
+// Estilos optimizados para ATS y similares al diseño original
+const styles = StyleSheet.create({
+  page: {
+    padding: 30,
+    fontSize: 10,
+    fontFamily: "Helvetica",
+    backgroundColor: "#ffffff",
+    color: "#000000",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 15,
+    gap: 15,
+  },
+  profileImage: {
+    width: 60,
+    height: 70,
+  },
+  headerContent: {
+    flex: 1,
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  contactInfo: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 5,
+  },
+  contactColumn: {
+    flex: 1,
+  },
+  contactText: {
+    fontSize: 9,
+    marginBottom: 3,
+  },
+  link: {
+    color: "#0066cc",
+    textDecoration: "underline",
+  },
+  section: {
+    marginBottom: 12,
+    borderTop: "0.5 solid #808080",
+  },
+  sectionTitle: {
+    fontSize: 10,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    textAlign: "center",
+    marginVertical: 6,
+    paddingVertical: 2,
+  },
+  text: {
+    fontSize: 9,
+    textAlign: "justify",
+    lineHeight: 1.4,
+  },
+  subsectionContainer: {
+    marginBottom: 4,
+  },
+  subsectionHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 3,
+  },
+  subsectionTitle: {
+    fontSize: 10,
+    fontWeight: "bold",
+  },
+  subsectionDate: {
+    fontSize: 8,
+    color: "#333333",
+  },
+  subsectionRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 2,
+  },
+  subsectionText: {
+    fontSize: 9,
+  },
+  contentIndent: {
+    marginLeft: 15,
+  },
+  list: {
+    marginLeft: 25,
+  },
+  listItem: {
+    fontSize: 9,
+    marginBottom: 2,
+    lineHeight: 1.3,
+  },
+  skillsList: {
+    marginLeft: 0,
+  },
+  skillItem: {
+    fontSize: 9,
+    marginBottom: 3,
+    lineHeight: 1.3,
+  },
+  bold: {
+    fontWeight: "bold",
+  },
+  languageRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 4,
+  },
+  stars: {
+    fontSize: 9,
+  },
+});
+
+interface ResumePDFProps {
+  data: any;
+}
+
+const ResumePDF: React.FC<ResumePDFProps> = ({ data }) => {
+  const { name, email, phone, website, location, summary } = data.basics;
+  const { address } = location;
+  const works: WorkType[] = data.work;
+  const educations: EducationType[] = data.education;
+  const programingLanguages: string[] = data.programmingLanguages;
+  const naturalLanguages: NaturalLanguageType[] = data.languages;
+  const techStacks = data.skills;
+  const personalProjects: ProjectType[] = data.projects;
+  const qualifications = data.interests;
+  const certificates = data.certificates;
+
+  // Función para renderizar estrellas como texto
+
+  const renderStars = (points: number) => {
+    const filled = "&#9733;".repeat(Math.floor(points)); // ★
+    const empty = "&#9734;".repeat(5 - Math.floor(points)); // ☆
+    return filled + empty;
+  };
+
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        {/* HEADER */}
+        <View style={styles.header}>
+          <Image src="/photo.jpg" style={styles.profileImage} />
+          <View style={styles.headerContent}>
+            <Text style={styles.name}>{name}</Text>
+            <View style={styles.contactInfo}>
+              <View style={styles.contactColumn}>
+                <Text style={styles.contactText}>Dirección: {address}</Text>
+                <Text style={styles.contactText}>
+                  Portafolio:{" "}
+                  <Link src={website} style={styles.link}>
+                    {website}
+                  </Link>
+                </Text>
+              </View>
+              <View style={styles.contactColumn}>
+                <Text style={styles.contactText}>
+                  Email:{" "}
+                  <Link src={`mailto:${email}`} style={styles.link}>
+                    {email}
+                  </Link>
+                </Text>
+                <Text style={styles.contactText}>Móvil: {phone}</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+
+        {/* RESUMEN */}
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Resumen</Text>
+          <Text style={styles.text}>{summary}</Text>
+        </View>
+
+        {/* FORMACIÓN ACADÉMICA */}
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Formación académica</Text>
+          {educations.map((edu) => (
+            <View key={edu.institution} style={styles.subsectionContainer}>
+              <View style={styles.subsectionHeader}>
+                <Text style={styles.subsectionTitle}>{edu.institution}</Text>
+                <Text style={styles.subsectionDate}>{edu.address}</Text>
+              </View>
+              <View style={styles.subsectionRow}>
+                <Text style={styles.subsectionText}>{edu.area}</Text>
+                <Text style={styles.subsectionDate}>
+                  {edu.startDate} - {edu.endDate}
+                </Text>
+              </View>
+            </View>
+          ))}
+        </View>
+
+        {/* HABILIDADES */}
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Habilidades</Text>
+          <View style={styles.skillsList}>
+            <Text style={styles.skillItem}>
+              <Text style={styles.bold}>- Lenguajes: </Text>
+              {programingLanguages.join(", ")}
+            </Text>
+            <Text style={styles.skillItem}>
+              <Text style={styles.bold}>- Frameworks: </Text>
+              {techStacks
+                .filter((stack) => stack.keywords?.includes("framework"))
+                .map((stack) => stack.name)
+                .join(", ")}
+            </Text>
+            <Text style={styles.skillItem}>
+              <Text style={styles.bold}>- Herramientas: </Text>
+              {techStacks
+                .filter((stack) => stack.keywords?.includes("tools"))
+                .map((stack) => stack.name)
+                .join(", ")}
+            </Text>
+            <Text style={styles.skillItem}>
+              <Text style={styles.bold}>- Plataformas: </Text>
+              {techStacks
+                .filter((stack) => stack.keywords?.includes("platform"))
+                .map((stack) => stack.name)
+                .join(", ")}
+            </Text>
+            <Text style={styles.skillItem}>
+              <Text style={styles.bold}>- Habilidades blandas: </Text>
+              {qualifications.map((q) => q.name).join(", ")}
+            </Text>
+          </View>
+        </View>
+
+        {/* EXPERIENCIA */}
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Experiencia</Text>
+          {works.map((work) => (
+            <View key={work.company} style={styles.subsectionContainer}>
+              <View style={styles.subsectionHeader}>
+                <View>
+                  <Text style={styles.subsectionTitle}>{work.company}</Text>
+                  <Text style={styles.subsectionText}>{work.position}</Text>
+                </View>
+                <Text style={styles.subsectionDate}>
+                  {work.startDate} - {work.endDate}
+                </Text>
+              </View>
+              <View style={styles.contentIndent}>
+                <Text style={styles.text}>{work.summary}</Text>
+                <View style={styles.list}>
+                  {work.highlights.map((highlight, idx) => (
+                    <Text key={idx} style={styles.listItem}>
+                      - {highlight}
+                    </Text>
+                  ))}
+                </View>
+              </View>
+            </View>
+          ))}
+        </View>
+
+        {/* PROYECTOS */}
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Proyectos</Text>
+          {personalProjects.map((project) => (
+            <View key={project.heading} style={styles.subsectionContainer}>
+              <Text style={styles.subsectionTitle}>{project.heading}</Text>
+              <View style={styles.contentIndent}>
+                <Text style={styles.text}>{project.summary}</Text>
+                <View style={styles.list}>
+                  {project.highlights.map((highlight, idx) => (
+                    <Text key={idx} style={styles.listItem}>
+                      - {highlight}
+                    </Text>
+                  ))}
+                </View>
+              </View>
+            </View>
+          ))}
+        </View>
+
+        {/* CERTIFICADOS */}
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Certificados</Text>
+          {certificates.map((cert) => (
+            <View key={cert.name} style={styles.subsectionContainer}>
+              <View style={styles.subsectionHeader}>
+                <View>
+                  <Text style={styles.subsectionTitle}>
+                    {cert.name} ({cert.issuer})
+                  </Text>
+                </View>
+                <Text style={styles.subsectionDate}>{cert.date}</Text>
+              </View>
+              <View style={styles.contentIndent}>
+                <View style={styles.list}>
+                  {cert.learning.map((item, idx) => (
+                    <Text key={idx} style={styles.listItem}>
+                      - {item}
+                    </Text>
+                  ))}
+                </View>
+              </View>
+            </View>
+          ))}
+        </View>
+
+        {/* IDIOMAS */}
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Idiomas</Text>
+          {naturalLanguages.map((lang) => (
+            <View key={lang.language} style={styles.languageRow}>
+              <Text style={styles.subsectionText}>{lang.language}</Text>
+              <Text style={styles.subsectionText}>{lang.fluency}</Text>
+            </View>
+          ))}
+        </View>
+      </Page>
+    </Document>
+  );
+};
+
+export default ResumePDF;
--- pages/curriculum-vitae.tsx
+++ pages/curriculum-vitae.tsx
@@ -1,85 +1,27 @@
-import Layout from "app/layouts/Layout";
+import { useState } from "react";
+import { Center } from "@chakra-ui/react";
 import Button from "ui/controls/Button/Button";
-import React, { useState } from "react";
-import {
-  Box,
-  Text,
-  Heading,
-  Stack,
-  HStack,
-  Center,
-  Image,
-  ListItem,
-  UnorderedList,
-  Spinner,
-} from "@chakra-ui/react";
-import StarRating from "react-svg-star-rating";
-import jsPDF, { jsPDFOptions } from "jspdf";
-import html2canvas from "html2canvas";
-import styles from "../styles/StarRating.module.css";
-
-import { WorkType, EducationType, NaturalLanguageType, ProjectType } from "../app/types";
+import Layout from "app/layouts/Layout";
+import ResumePDF from "../components/resume-pdf";
 import data from "../data/resume.json";
-import { H3, H4, TimeSpan, TitleItem } from "../components/Resume";
+import { pdf } from "@react-pdf/renderer";
 
 const Curriculum = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const { name, email, phone, website, location } = data.basics;
-  const { address } = location;
-
-  const summary: string = data.basics.summary;
-  const works: WorkType[] = data.work;
-  const educations: EducationType[] = data.education;
-  const programingLanguages: string[] = data.programmingLanguages;
-  const naturalLanguages: NaturalLanguageType[] = data.languages;
-  const techStacks = data.skills;
-  const personalProjects: ProjectType[] = data.projects;
-  const qualifications = data.interests;
-  const certificates = data.certificates;
-
-  const options: jsPDFOptions = {
-    orientation: "p",
-    unit: "pt",
-    format: "a4",
-  };
-
   const createPDF = async () => {
     try {
       setIsLoading(true);
-      const pdf = new jsPDF(options);
-      const element = document.querySelector("#curriculum-vitae") as HTMLElement;
-      const pageWidth = pdf.internal.pageSize.getWidth();
-      const pageHeight = pdf.internal.pageSize.getHeight();
-      const margin = 10;
-      const contentWidth = pageWidth - 2 * margin;
-      const contentHeight = pageHeight - 2 * margin + 520;
-
-      const totalHeight = element.scrollHeight; // Cambiar a scrollHeight para obtener la altura total
-      const pageCount = Math.ceil(totalHeight / contentHeight);
-
-      for (let i = 0; i < pageCount; i++) {
-        if (i > 0) pdf.addPage();
-
-        const canvas = await html2canvas(element, {
-          scale: 1.5, // Aumentar la escala para mejor calidad
-          y: i * contentHeight,
-          height: contentHeight, // Añadir 10px para evitar cortes
-          windowWidth: element.scrollWidth,
-          windowHeight: totalHeight, // Usar scrollHeight para asegurar la captura total
-        });
-
-        const imgData = canvas.toDataURL("image/png", 0.85);
-        const imgProps = pdf.getImageProperties(imgData);
-        const imgHeight = (imgProps.height * contentWidth) / imgProps.width; // Mantener la relación de aspecto
-
-        pdf.addImage(imgData, "PNG", 0, margin, contentWidth, imgHeight);
-      }
-
-      pdf.save("curriculum-Lescano-Jhon.pdf");
+      const blob = await pdf(<ResumePDF data={data} />).toBlob();
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "curriculum-Lescano-Jhon.pdf";
+      link.click();
+      URL.revokeObjectURL(url);
     } catch (error) {
       console.error("Error al generar PDF:", error);
     } finally {
-      setIsLoading(false); // 🔹 Ocultar spinner
+      setIsLoading(false);
     }
   };
 
@@ -88,213 +30,18 @@ const Curriculum = () => {
       description="I believe in web development as a powerful communication tool to reflect life projects, communicate ventures and ideas."
       title="Curriculum Vitae - Jhon Lescano"
     >
-      <Center display={{ base: "none", lg: "flex" }} flexDir="column" gap={4}>
+      <Center
+        display={{ base: "none", lg: "flex" }}
+        flexDir="column"
+        gap={4}
+        h={"full"}
+        alignItems={"center"}
+      >
         <Button
           text={isLoading ? "Generando PDF..." : "Descargar CV en PDF"}
           onClick={() => createPDF()}
           enabled={isLoading}
         />
-        {isLoading && (
-          <HStack>
-            <Spinner size="sm" color="blue.500" />
-            <Text fontSize="sm" color="gray.600">
-              Generando PDF, por favor espera...
-            </Text>
-          </HStack>
-        )}
-      </Center>
-      <Center display={{ base: "none", lg: "flex" }} id="curriculum-vitae">
-        <Stack bg="#ffffff" color="#000000" m={4} p={8} width="866px" fontSize={"xs"}>
-          <Stack as={"header"} gap={4} display={"flex"} flexDir={"row"} fontSize={"xs"}>
-            <Image w={24} h={24} src="/photo.jpg" alt="Jhon Lescano" objectFit={"cover"} />
-            <Box w={"full"}>
-              <Heading as="h2" fontSize="2xl" letterSpacing="wider">
-                {name}
-              </Heading>
-              <Box
-                w="100%"
-                display={"flex"}
-                alignItems="center"
-                justifyContent="space-between"
-                marginY="3"
-              >
-                <Box>
-                  <Text marginBottom={1}>Dirección: {address}</Text>
-                  <Text>
-                    Portafolio:{" "}
-                    <a
-                      href={website}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      style={{ color: "blue", textDecoration: "underline" }}
-                    >
-                      {website}
-                    </a>
-                  </Text>
-                </Box>
-                <Box>
-                  <Text marginBottom={1}>
-                    Email:{" "}
-                    <a
-                      href={`mailto:${email}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      style={{ color: "blue", textDecoration: "underline" }}
-                    >
-                      {email}
-                    </a>
-                  </Text>
-                  <Text>Móvil: {phone}</Text>
-                </Box>
-              </Box>
-            </Box>
-          </Stack>
-          <Box>
-            <H3 content="Resumen" />
-            <Text textAlign={"justify"}>{summary}</Text>
-          </Box>
-          <Box>
-            <H3 content="Formación académica" />
-            {educations.map(({ institution, area, startDate, endDate, address }) => (
-              <Stack key={institution} gap={0}>
-                <HStack justifyContent={"space-between"} alignItems={"center"}>
-                  <H4 content={institution} />
-                  <Text>{address}</Text>
-                </HStack>
-                <HStack justifyContent={"space-between"} alignItems={"center"}>
-                  <Text>{area}</Text>
-                  <TimeSpan endDate={endDate} startDate={startDate} />
-                </HStack>
-              </Stack>
-            ))}
-          </Box>
-          <Box>
-            <H3 content="Habilidades" />
-            <UnorderedList listStyleType={"none"}>
-              <ListItem>
-                <TitleItem>- Lenguajes: </TitleItem>
-                {programingLanguages.map((language, indexLanguage) => (
-                  <span key={language}>
-                    {language}
-                    {indexLanguage === programingLanguages.length - 1 ? "" : ", "}
-                  </span>
-                ))}
-              </ListItem>
-              <ListItem>
-                <TitleItem>- Frameworks: </TitleItem>
-                {techStacks
-                  .filter((stack) => stack.keywords?.includes("framework"))
-                  .map((stack) => stack.name)
-                  .join(", ")}
-              </ListItem>
-              <ListItem>
-                <TitleItem>- Herramientas: </TitleItem>
-                {techStacks
-                  .filter((stack) => stack.keywords?.includes("tools"))
-                  .map((stack) => stack.name)
-                  .join(", ")}
-              </ListItem>
-              <ListItem>
-                <TitleItem>- Plataformas: </TitleItem>
-                {techStacks
-                  .filter((stack) => stack.keywords?.includes("platform"))
-                  .map((stack) => stack.name)
-                  .join(", ")}
-              </ListItem>
-              <ListItem>
-                <TitleItem>- Habilidades blandas: </TitleItem>
-                {qualifications.map((qualification, indexQualification) => (
-                  <span key={qualification.name}>
-                    {qualification.name}
-                    {indexQualification === qualifications.length - 1 ? "" : ", "}
-                  </span>
-                ))}
-              </ListItem>
-            </UnorderedList>
-          </Box>
-          <Box>
-            <H3 content="Experiencia" />
-            {works.map(({ company, position, startDate, endDate, summary, highlights }) => (
-              <Stack key={company} marginBottom={2}>
-                <HStack justifyContent={"space-between"} alignItems={"start"}>
-                  <Box>
-                    <H4 content={company} />
-                    <Text>{position}</Text>
-                  </Box>
-                  <TimeSpan endDate={endDate} startDate={startDate} />
-                </HStack>
-                <Box ml={8}>
-                  <Text marginY="1">{summary}</Text>
-                  <UnorderedList ml={12} listStyleType={"none"}>
-                    {highlights.map((highlight) => (
-                      <ListItem key={highlight}>- {highlight}</ListItem>
-                    ))}
-                  </UnorderedList>
-                </Box>
-              </Stack>
-            ))}
-          </Box>
-          <Box>
-            <H3 content="Proyectos" />
-            {personalProjects.map(({ heading, summary, highlights }) => (
-              <Stack key={heading} marginBottom={2}>
-                <H4 content={heading} />
-                <Box ml={8}>
-                  <Text marginY="1">{summary}</Text>
-                  <UnorderedList ml={12} listStyleType={"none"}>
-                    {highlights.map((highlight) => (
-                      <ListItem key={highlight}>- {highlight}</ListItem>
-                    ))}
-                  </UnorderedList>
-                </Box>
-              </Stack>
-            ))}
-          </Box>
-          <Box>
-            <H3 content="Certificados" />
-            {certificates.map(({ name, date, learning, issuer }) => (
-              <Stack key={name} marginBottom={2}>
-                <HStack justifyContent={"space-between"} alignItems={"start"}>
-                  <Box>
-                    <H4 content={name} />
-                    <Text display={"inline"} ml={1} fontWeight="semibold">
-                      ({issuer})
-                    </Text>
-                  </Box>
-                  <TimeSpan startDate={date} />
-                </HStack>
-                <Box ml={8}>
-                  <UnorderedList listStyleType={"none"}>
-                    {learning.map((highlight) => (
-                      <ListItem key={highlight}>- {highlight}</ListItem>
-                    ))}
-                  </UnorderedList>
-                </Box>
-              </Stack>
-            ))}
-          </Box>
-          <Box>
-            <H3 content="Idiomas" />
-            {naturalLanguages.map(({ language, fluency, points }) => (
-              <HStack key={language} justifyContent="space-between">
-                <Text>{language}</Text>
-                <HStack>
-                  <Text>{fluency}</Text>
-                  <Box>
-                    <StarRating
-                      isReadOnly
-                      activeColor={"#000000"}
-                      containerClassName={styles.star}
-                      initialRating={points}
-                      size={15}
-                      unit="float"
-                    />
-                  </Box>
-                </HStack>
-              </HStack>
-            ))}
-          </Box>
-        </Stack>
       </Center>
     </Layout>
   );
